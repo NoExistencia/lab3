e1cedb5c4f176379a97b3fe115cadc7e3dfae52e
alumnoFI <alumnoFI@FILC007-19>
2024-09-05 22:23:47 -0300

    cambios

---
+++ lab3/Pais.cs
@@ -0,0 +1,12 @@
+﻿namespace Semana3
+{
+    public class Pais
+    {
+        public int Id { get; set; }
+        public static int UltimoId { get; set; } = 1;
+        public string Codigo { get; set; }
+        public string NombrePais { get; set; }
+     
+
+    }
+}
---
+++ lab3/Pelicula.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana3
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public static int UltimoId { get; set; } = 1;
+        public string Titulo { get; set; }
+        public Genero Genero { get; set; }
+        public Director Director { get; set; }
+        public DateTime FchLanzamiento { get; set; }
+        public int Duracion { get; set; }
+
+        //public string TipoCombustible { get; set; }
+
+
+        //  public Auto()
+        //{
+        //  Id = UltimoId;
+        //UltimoId++;
+        //FechaConstruccion = DateTime.Now;
+        //}
+
+        public Pelicula(string titulo, Genero genero, Director director, DateTime fechalanzamiento, int duracion)
+        {
+            Id = UltimoId;
+            UltimoId++;
+            Titulo = titulo;
+            Genero = genero;
+            Director = director;
+            FchLanzamiento = fechalanzamiento;
+            Duracion = duracion;
+        }
+
+        public Pelicula()
+        {
+            Id = UltimoId;
+            UltimoId++;
+        }
+
+        public DateTime fchbaja()
+        {
+            var fecha = this.FchLanzamiento;
+            fecha = fecha.AddDays(90);
+            return fecha;
+        }
+
+        public bool esApta()
+        {
+            var gen = this.Genero;
+            if (gen == Genero.terror || gen == Genero.thriller || gen == Genero.suspenso)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
+
+
+
+        //public Auto(string marca, DateTime fechaConstruccion)
+        //{
+        //    Id = UltimoId;
+        //    UltimoId++;
+        //    Marca = marca;
+        //    FechaConstruccion = fechaConstruccion;
+        //}
